a7737989928030c7bf1f8835ac363eb021a6d01c
André Fettouhi <A.Fettouhi@gmail.com>
2020-05-17 06:18:25 +0200

    Initial commit!

---
+++ Chapter 1/Chapter-1.py
@@ -0,0 +1,25 @@
+for countdown in 5, 4, 3, 2, 1, "hey!":
+    print(countdown)
+
+cliches = [
+    "At the end of the day",
+    "Having said that",
+    "The fact of the matter is",
+    "Be that as it may",
+    "The bottom line is",
+    "If you will",
+    ]
+print(cliches[3])
+
+quotes = {
+    "Moe": "A wise guy, huh?",
+    "Larry": "Ow!",
+    "Curly": "Nyuk nyuk!",
+    }
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
+
+language = 7
+print("Language %s: I am Python. What's for supper?" % language)
+
+import this
