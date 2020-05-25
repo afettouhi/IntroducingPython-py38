40b68676b27d8abbd4a34950af2b934143e0c3b9
André Fettouhi <A.Fettouhi@gmail.com>
2020-05-25 06:38:49 +0200

    Added examples for Appendix F!

---
+++ Appendix F.py
@@ -0,0 +1,70 @@
+s = "OH, my paws and whiskers!"
+t = "I'm late!"
+
+s.capitalize()
+
+s.lower()
+
+s.swapcase()
+
+s.title()
+
+s.upper()
+
+s.count('w')
+
+s.find('w')
+
+s.index('w')
+
+s.rfind('w')
+
+s.rindex('w')
+
+s.startswith('OH')
+
+''.join(s)
+
+' '.join(s)
+
+' '.join((s, t))
+
+s.lstrip('HO')
+
+s.replace('H', 'MG')
+
+s.rsplit()
+
+s.rsplit(' ', 1)
+
+s.split()
+
+s.split(' ')
+
+s.splitlines()
+
+s.strip()
+
+s.strip('s!')
+
+s.center(30)
+
+s.expandtabs()
+
+s.ljust(30)
+
+s.rjust(30)
+
+s.isalnum()
+
+s.isalpha()
+
+s.isprintable()
+
+s.istitle()
+
+s.isupper()
+
+s.isdecimal()
+
+s.isnumeric()
